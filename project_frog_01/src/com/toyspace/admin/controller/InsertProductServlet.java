12f01a4910d3e6176e4d58f6a608fd9b94b993fb
home <home@DESKTOP-EKIR98E>
2020-10-08 13:19:41 +0900

    admin_header - 제품등록, 회원관리, 상품등록  페이지 연결 ( 경록 )

--- project_frog_01/src/com/toyspace/admin/controller/InsertProductServlet.java
+++ project_frog_01/src/com/toyspace/admin/controller/InsertProductServlet.java
@@ -28,7 +28,7 @@ public class InsertProductServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/views/admin/productEnroll.jsp").forward(request, response);
 	}
 
 	/**
--- project_frog_01/src/com/toyspace/admin/controller/ManageMemberServlet.java
+++ project_frog_01/src/com/toyspace/admin/controller/ManageMemberServlet.java
@@ -27,7 +27,7 @@ public class ManageMemberServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/views/admin/memberlist.jsp").forward(request, response);
 	}
 
 	/**
