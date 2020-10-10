06ecfd7045754773415c7aa57f4a1d5100b65744
home <home@DESKTOP-EKIR98E>
2020-10-10 19:59:57 +0900

    admin ManageStockServlet 경로 오타 수정

--- project_frog_01/src/com/toyspace/admin/controller/ManageStockServlet.java
+++ project_frog_01/src/com/toyspace/admin/controller/ManageStockServlet.java
@@ -27,7 +27,7 @@ public class ManageStockServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.getRequestDispatcher("/views/admin/sto.jsp").forward(request, response);
+		request.getRequestDispatcher("/views/admin/stock.jsp").forward(request, response);
 	}
 
 	/**
