5e3b8f0579f073c454bb44d67f2bdd9abfdc1f33
Byung-hyun Yoon <mightymosses@gmail.com>
2020-10-12 16:39:11 +0900

    결제 관련 jar 파일 추가/결제 모듈 수정중

---
+++ project_frog_01/src/com/toyspace/order/controller/OrderCompleteServlet.java
@@ -0,0 +1,44 @@
+package com.toyspace.order.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class OrderCompleteServlet
+ */
+@WebServlet("/order/complete")
+public class OrderCompleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public OrderCompleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String impUid = request.getParameter("imp_uid");
+		String merchantUid = request.getParameter("merchant_uid");
+		System.out.println(impUid);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
