c1069eef396ed93deaf8324f1759a5e06369af02
Byung-hyun Yoon <mightymosses@gmail.com>
2020-10-16 03:38:46 +0900

    어드민에 그동안 비어있던 서블릿 연결

--- project_frog_01/src/com/toyspace/admin/controller/ManageChatServlet.java
+++ project_frog_01/src/com/toyspace/admin/controller/ManageChatServlet.java
@@ -27,7 +27,7 @@ public class ManageChatServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/views/admin/adminChat.jsp").forward(request, response);
 	}
 
 	/**
